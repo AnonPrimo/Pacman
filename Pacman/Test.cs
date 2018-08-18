77a909d5c7304e4006b6088fcc6de9afa18705dc
AnonPrimo <esichkaisi@gmail.com>
2018-08-18 13:47:27 +0300

    aert

---
+++ Pacman/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    class Test
+    {
+
+        Bitmap bt = new Bitmap("pacman.png");
+        Graphics g;
+        Rectangle r;
+
+
+    }
+}
---
+++ PacmenLabirint/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacmenLabirint
+{
+    public partial class Form1 : Form
+    {
+        int[,] ar = new int[10, 10];
+
+        Rectangle [,]rect = new Rectangle[10,10];
+        Graphics g = new Graphics();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    rect[i, j] = new Rectangle(i * 50, j * 50, 50, 50);
+                    
+                }
+            }
+        }
+    }
+}

5a52e05a4085e32a1545eeec70db288ee5a338d5
AnonPrimo <esichkaisi@gmail.com>
2018-08-19 21:25:57 +0300

    edit

--- PacmenLabirint/Form1.cs
+++ PacmenLabirint/Form1.cs
@@ -13,7 +13,18 @@ namespace PacmenLabirint
     public partial class Form1 : Form
     {
         Random r;
-        int[,] ar = new int[10, 10];
+        int[,] ar = { 
+        { 1, 1, 1, 1, 1, 0, 0, 0, 1, 1 },
+        { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+        { 1, 0, 1, 2, 1, 0, 1, 1, 1, 0 },
+        { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 1, 0, 1, 1, 1, 1, 0, 1 },
+        { 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+        { 0, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 1, 1, 1, 0 },
+        { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 }
+        };
 
         Rectangle [,]rect = new Rectangle[10,10];
         Graphics g;
@@ -28,8 +39,8 @@ namespace PacmenLabirint
 
             this.g = this.CreateGraphics();
             r = new Random();
+            pacmen = new Rectangle(50 + 5, 50 + 5, 35, 35);
 
-            pacmen = new Rectangle(0, 0, 50, 50);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -51,53 +62,86 @@ namespace PacmenLabirint
             //    }
             //}
 
+            
 
 
-
-            ///for fixed wall;
+            /////for fixed wall;
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
                     rect[i, j] = new Rectangle(i * 50, j * 50, 50, 50);
+
+                    if (ar[i, j] == 1)
+                    {
+                        g.FillRectangle(Brushes.Orange, rect[i, j]);
+                    }
+                    //if (ar[i, j] == 2)
+                    //    pacmen = new Rectangle(i * 50 + 5, j * 50 + 5, 35, 35);
+
                 }
             }
+
+
+
             g.FillRectangle(Brushes.Yellow, pacmen);
-
-            g.FillRectangle(Brushes.Orange, rect[2, 3]);
-            g.FillRectangle(Brushes.Orange, rect[2, 4]);
-            g.FillRectangle(Brushes.Orange, rect[3, 4]);
-
-
 
         }
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            if (e.KeyChar == '2')
+                pacmen.Y += 1;
 
+            if (e.KeyChar == '8')
+                pacmen.Y -= 1;
+
+            if (e.KeyChar == '4')
+                pacmen.X -= 1;
+                
+            if (e.KeyChar == '6')               
+                pacmen.X += 1;
+
+            if (Check())
+            {
+                if (e.KeyChar == '2')
+                    pacmen.Y -= 2;
+
+                if (e.KeyChar == '8')
+                    pacmen.Y += 2;
+
+                if (e.KeyChar == '4')
+                    pacmen.X += 2;
+
+                if (e.KeyChar == '6')
+                    pacmen.X -= 2;
+            }
+
+            Refresh();
+        }
+
+
+        private bool Check()
+        {
 
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
+                    if (ar[i, j] == 1)
+                    {
+                        if ((Math.Abs((pacmen.X + 15) - (rect[i, j].X + 25)) < 40) && (Math.Abs((pacmen.Y + 15) - (rect[i, j].Y + 25)) < 40))
+                            return true;
+                            //if ((pacmen.X > rect[i, j].X + 50) || (pacmen.X + 50 < rect[i, j].X) || (pacmen.Y < rect[i, j].Y + 50) || (pacmen.Y + 50 > rect[i, j].Y))
 
+
+                            
+                    }  
                 }
             }
 
+            return false; //врізається 
 
-            if (e.KeyChar == 's')
-                pacmen.Y += 5;
-
-            if (e.KeyChar == 'w')
-                pacmen.Y -= 5;
-
-            if (e.KeyChar == 'a')
-                pacmen.X -= 5;
-
-            if (e.KeyChar == 'd')
-                pacmen.X += 5;
-
-            Refresh();
         }
     }
 }

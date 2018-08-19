f6c8d40d190f886de2675effbd0c4e484cb2294a
Jul-ia <julie78sd@gmail.com>
2018-08-19 16:13:40 +0300

    em//

--- PacmenLabirint/Form1.cs
+++ PacmenLabirint/Form1.cs
@@ -15,18 +15,21 @@ namespace PacmenLabirint
         int[,] ar = new int[10, 10];
 
         Rectangle [,]rect = new Rectangle[10,10];
-        Graphics g = new Graphics();
+        Graphics g;
 
 
         public Form1()
         {
             InitializeComponent();
+
+            this.g = this.CreateGraphics();
+
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
                     rect[i, j] = new Rectangle(i * 50, j * 50, 50, 50);
-                    
+                    g.DrawRectangle(Pens.Aquamarine, rect[i, j]);
                 }
             }
         }
